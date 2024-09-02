c854342d5a6b6579813072a0abeb6d888d9c45fb
KunalPusdekar <kunalpusdekar16@gmail.com>
2024-09-02 20:30:43 +0530

    Added css to Pokedex

--- pokedex/src/components/PokemonList/PokemonList.jsx
+++ pokedex/src/components/PokemonList/PokemonList.jsx
@@ -52,11 +52,14 @@ function PokemonList(){
     
     return (
         <div className='pokemon-list-wrapper'>
-           Pokemon List
-           <div>
+           <div class="pokemon-wrapper">
            {(isLoading)?'Loading...':
                pokemonList.map((p)=> <Pokemon name={p.name} image={p.image} key={p.id}/>)
             }
+           </div>
+           <div className='controls'>
+            <button>Prev</button>
+            <button>Next</button>
            </div>
         </div>
     )
