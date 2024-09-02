2dd383fcd07b40da1ef39a0e77414df7c39456b2
KunalPusdekar <kunalpusdekar16@gmail.com>
2024-09-02 22:04:40 +0530

    added pagination to Pokedex

--- pokedex/src/components/PokemonList/PokemonList.jsx
+++ pokedex/src/components/PokemonList/PokemonList.jsx
@@ -8,16 +8,19 @@ function PokemonList(){
     const [pokemonList,setPokemonList]  = useState([]);
     const [isLoading,setIsLoading] = useState(true);
 
-    const POKEDEX_URL = 'https://pokeapi.co/api/v2/pokemon';
+    const [pokedexUrl,setPokedexUrl] = useState('https://pokeapi.co/api/v2/pokemon');
 
-
+    const [nextUrl,setNextUrl]= useState('');
+    const [prevUrl,setPrevUrl]= useState('');
     async function downloadPokemons(){
-
-        const response = await axios.get(POKEDEX_URL);//this downloads list of 20 pokemons
+        setIsLoading(true);
+        const response = await axios.get(pokedexUrl);//this downloads list of 20 pokemons
 
         const pokemonResults = response.data.results;//we get the array of pokemons from result
 
         console.log(response.data);
+        setNextUrl(response.data.next);
+        setPrevUrl(response.data.previous);
 
         //iterating ovet the array of pokemons, and using their url, to  create an array of promises
         // that  will downlead those 20 pokemons
@@ -44,7 +47,7 @@ function PokemonList(){
     }
     useEffect(()=>{
        downloadPokemons();
-    },[]); 
+    },[pokedexUrl]); 
      // if the array is 
      //             1)empty => not executed for any rendering of components
      //             2)one element =>execute for that perticular element rendering
@@ -58,8 +61,8 @@ function PokemonList(){
             }
            </div>
            <div className='controls'>
-            <button>Prev</button>
-            <button>Next</button>
+            <button disabled={prevUrl == null} onClick={()=>setPokedexUrl(prevUrl)}>Prev</button>
+            <button disabled={nextUrl == null}  onClick={()=>setPokedexUrl(nextUrl)}>Next</button>
            </div>
         </div>
     )
