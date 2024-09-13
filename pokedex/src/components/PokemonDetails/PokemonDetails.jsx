7133353129f7a476b02504046dbb2a006d5d97ee
KunalPusdekar <kunalpusdekar16@gmail.com>
2024-09-13 19:05:35 +0530

    Added the custom hooks

--- pokedex/src/components/PokemonDetails/PokemonDetails.jsx
+++ pokedex/src/components/PokemonDetails/PokemonDetails.jsx
@@ -1,26 +1,11 @@
 
-import axios from  "axios";
 import { useParams } from "react-router-dom";
-import { useEffect,useState } from "react";
 import './PokemonDetails.css'
+import usePokemonDetails from "../../hooks/usePokemonDetails";
 function PokemonDetails(){
    const {id}=useParams();
-   const [pokemon,setPokemon]=useState({});
-   async function downloadPokemon(){;
-    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
-    console.log(response);
-    setPokemon({
-        name:response.data.name,
-        image:response.data.sprites.other.dream_world.front_default,
-        weight:response.data.weight,
-        height:response.data.height,
-        types:response.data.types.map((t)=>t.type.name)
-    })
-   }
+   const [pokemon]= usePokemonDetails(id);
 
-   useEffect(()=>{
-          downloadPokemon();
-   },[]);
    return (
       <div className="pokemon-details-wrapper">
         <img className="pokemon-details-image" src={pokemon.image}/>
@@ -30,6 +15,14 @@ function PokemonDetails(){
         <div className="pokemon-details-types">
             {pokemon.types && pokemon.types.map((t)=> <div key={t}>{t}</div>)}
         </div>
+        {   pokemon.types && pokemon.similarPokemons &&
+        <div>
+           More {pokemon.types[0]} type pokemons
+           <ul>
+            {pokemon.similarPokemons && pokemon.similarPokemons.map((p)=> <li key={p.pokemon.id}>{p.pokemon.name}</li>) }
+           </ul>
+        </div>
+        }
 
       </div>
    )
