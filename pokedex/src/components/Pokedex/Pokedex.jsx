b829c10b2df507c21e0b7ea0ab8e5aa666010802
KunalPusdekar <kunalpusdekar16@gmail.com>
2024-09-21 15:06:58 +0530

    Adding new custom hook ->useDebounce

--- pokedex/src/components/Pokedex/Pokedex.jsx
+++ pokedex/src/components/Pokedex/Pokedex.jsx
@@ -1,12 +1,17 @@
+import { useEffect, useState } from "react";
 import PokemonList from "../PokemonList/PokemonList";
 import Search from "../Search/Search";
 import "./Pokedex.css"
+import PokemonDetails from "../PokemonDetails/PokemonDetails";
 function Pokedex(){
+
+    const [searchTerm,setSearchterm] = useState('');
 
     return(
         <div className="pokedex-wrapper">
-        <Search/>
-        <PokemonList/>
+        <Search updateSearchTerm={setSearchterm}/>
+        {searchTerm.length==0 ? <PokemonList/>:<PokemonDetails key={searchTerm}  pokemonName={searchTerm}/>} 
+
         </div>
     )
 }
--- pokedex/src/components/Search/Search.jsx
+++ pokedex/src/components/Search/Search.jsx
@@ -1,8 +1,18 @@
+import { useState } from "react";
 import "./Search.css"
-function Search(){
+import useDebounce from "../../hooks/useDebounce";
+
+function Search({updateSearchTerm}){
+
+   const debouncedCallback = useDebounce((e)=>updateSearchTerm(e.target.value))
    return(
     <div className="search-wrapper">
-    <input id="pokemon-name-search" type="text"  placeholder="Pokemon name..." />
+    <input 
+       id="pokemon-name-search" 
+       type="text"  
+       placeholder="Pokemon name..." 
+       onChange={debouncedCallback}
+       />
     </div>
    )
 }
